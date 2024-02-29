59197224fa503117a765856f1e03d0e79efeec8c
Nutchapon <NUTCHAPON.M@ach.co.th>
2024-02-29 09:09:50 +0700

    update code template 2024-02-29

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:alpine as builder
+WORKDIR /go/src
+COPY . .
+RUN go get
+RUN go build -o /go/bin/app_api
+
+FROM alpine
+RUN apk update && apk add --no-cache tzdata
+COPY --from=builder /go/bin/app_api /app_api
+COPY --from=builder /go/src/config.yaml /config.yaml
+ENV TZ=Asia/Bangkok
+ENTRYPOINT [ "/app_api" ]
--- main.go
+++ main.go
@@ -1,29 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"go-hexagonal-architecture/repository"
 	"go-hexagonal-architecture/routes"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
+	initTimeZone()
+	initConfig()
+
 	app := fiber.New()
 
 	db := initDataBase()
 
 	app.Mount("/api/auth", routes.Auth(db))
 
-	app.Listen(":8080")
+	app.Listen(fmt.Sprintf(":%v", viper.GetInt("app.port")))
+}
+
+func initTimeZone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+	time.Local = ict
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func initDataBase() *gorm.DB {
 
-	dsn := "host=localhost user=myuser password=mypassword dbname=mydb port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	dsn := fmt.Sprintf(
+		"host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Bangkok",
+		viper.GetString("db.host"),
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.dbname"),
+		viper.GetInt("db.port"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		DryRun: true, // ถ้ารค่าเป็น true จะแสดงเฉพาะ sql statment
+		DryRun: false, // ถ้ารค่าเป็น true จะแสดงเฉพาะ sql statment
 	})
 	if err != nil {
 		panic(err)
